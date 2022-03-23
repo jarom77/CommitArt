72ec6c9a29539d40e02f520127dc601d8431aa71
Jarom Christensen <jarom77@gmail.com>
2022-03-23 11:25:59 -0600

    Commented functions

--- draw.sh
+++ draw.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# determine row and column from index
 # Dec 29 2019 is a Sunday, and is the beginning reference
 timezone=$(date +%z --date="$1")
 secondsAfter=$[$(date +%s --date="$1") - $(date +%s --date="Dec 29, 2019 00:00 $timezone")]
@@ -6,6 +8,14 @@ secondsAfter=$[$(date +%s --date="$1") - $(date +%s --date="Dec 29, 2019 00:00 $
 ((row=index % 7))
 ((column=index / 7))
 
+
+# determine index of pattern list; determines slope by region:
+# +----+----+     +---+---+ 
+# | 1  | -1 |     | / | \ | 
+# +----+----+ ->  +---+---+ 
+# | -1 | 1  |     | \ | / | 
+# +----+----+     +---+---+ 
+# This creates a diamond structure
 listIndex=
 if [ $[$column % 12 / 6] -eq $[$row / 4] ]; then
 	((listIndex=column+row))
@@ -13,14 +23,17 @@ else
 	((listIndex=column-row))
 fi
 
+# pattern is repeated infinitely and indexed by listIndex
 pattern=123432
 length=6
 ((listIndex=(listIndex+length)%length+1))
 fillIndex=$(echo $pattern | head -c $listIndex | tail -c +$listIndex)
 
+# commit fillIndex times on given date
 count=0
 while [ $count -lt $fillIndex ]
 do
 	((count=count+1))
+	# create empty commits on given date
 	git commit --allow-empty --date="$1" -m "Commit Art"
 done
--- year.sh
+++ year.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+
+# move to containing directory
 cd $(dirname "$0")
+
+# read in year from user
 echo Enter year to populate:
 read year
+
+# set start date as Jan 1 {year} at 2:00 am to avoid DST problems
 startDate=$(date +%s --date="Jan 1 $year")
 ((startDate=startDate+3600*2))
 
+# confirmation messages
 echo "Make sure you are on the master branch [ENTER]"
 read null
 
@@ -13,6 +20,7 @@ echo "This is basically irreversable....."
 read answer
 [ "$answer" != "YES" ] && exit
 
+# runs draw.sh for each start date, shifting by i days each time
 for i in {0..365}
 do
 	bash draw.sh "@$[$startDate+$i*24*3600]"
