6ce73aaf97000c9cc1daaa6a1b8f7f6db86f7591
Jarom Christensen <jarom77@gmail.com>
2022-03-23 01:23:29 -0600

    Finished and tested code

--- draw.sh
+++ draw.sh
@@ -5,10 +5,22 @@ secondsAfter=$[$(date +%s --date="$1") - $(date +%s --date="Dec 29, 2019 00:00 $
 ((index=secondsAfter/60/60/24))
 ((row=index % 7))
 ((column=index / 7))
-echo $row,$column
 
+listIndex=
 if [ $[$column % 12 / 6] -eq $[$row / 4] ]; then
-	echo positive
+	((listIndex=column+row))
 else
-	echo negative
+	((listIndex=column-row))
 fi
+
+pattern=123432
+length=6
+((listIndex=(listIndex+length)%length+1))
+fillIndex=$(echo $pattern | head -c $listIndex | tail -c +$listIndex)
+
+count=0
+while [ $count -lt $fillIndex ]
+do
+	((count=count+1))
+	git commit --allow-empty --date="$1" -m "Commit Art"
+done
